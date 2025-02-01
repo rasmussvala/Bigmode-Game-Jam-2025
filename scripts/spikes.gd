4be6adff2df66f67967d230f9b1c78a973d3f532
Rasmus Svala <rasmus.svala@hotmail.com>
2025-02-01 19:10:16 +0100

    spike clean up

--- scripts/spikes.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-
-@onready var polygon_2d: Polygon2D = $Polygon2D
-@onready var collision_polygon_2d: CollisionPolygon2D = $CollisionPolygon2D
-
-func _ready():
-	# make collision polygon match polygon shape
-	collision_polygon_2d.polygon = polygon_2d.polygon
